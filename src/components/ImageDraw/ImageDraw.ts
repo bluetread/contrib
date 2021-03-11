0bea5d3780b6ef8ce011c32ddd5aa998e46c645c
anvelez26 <anvelez26@gmail.com>
2021-03-10 23:39:43 -0500

    canvas updates

--- src/components/ImageDraw/ImageDraw.ts
+++ src/components/ImageDraw/ImageDraw.ts
@@ -1,5 +1,5 @@
 import { Components } from 'formiojs';
-const FileComponent = (Components as any).components.field;
+const FileComponent = (Components as any).components.file;
 
 export default class ImageDrawComponent extends FileComponent {
     public static schema() {
@@ -22,26 +22,52 @@ export default class ImageDrawComponent extends FileComponent {
     }
 
     render(){
-        return super.render(this.renderTemplate('imagedraw', {
-            fileSize: this.fileSize,
-            files: this.dataValue || [],
-            statuses: this.statuses,
-            disabled: this.disabled,
-            support: this.support,
-            fileDropHidden: this.fileDropHidden
-        }));
+        let uploadRender = super.render();
+        uploadRender += '<canvas ref="drawpad" height="200px" width="200px></canvas>';
+        return uploadRender;
+        // return super.render(this.renderTemplate('imagedraw', {
+        //     fileSize: this.fileSize,
+        //     files: this.dataValue || [],
+        //     statuses: this.statuses,
+        //     disabled: this.disabled,
+        //     support: this.support,
+        //     fileDropHidden: this.fileDropHidden
+        // }));
+    }
+
+    getValue(){
+        return this.dataValue;
+    }
+
+    get emptyValue(){
+        return[];
+    }
+
+    get modes(){
+        return{
+            pencil:{
+                title: 'Pencil',
+                state:{
+                    mode: 'pencil'
+                },
+                
+            }
+        }
     }
 
     attach(element){
         const attachRet = super.attach(element);
         this.loadRefs(element.parentNode, {
-            drawpad: 'multiple'
+            drawpad: 'single'
         });
-        if (this.refs.drawpad && this.refs.drawpad.length){
-            const ctx = this.refs.drawpad[0].getContext("2d");
-            ctx.fillStyle = "#FF0000";
-            ctx.fillRect(0, 0, 150, 75);
-        }
+        const ctx = this.refs.drawpad.getContext("2d");
+              ctx.fillStyle = "#FF0000";
+              ctx.fillRect(0, 0, 150, 75);
+        // if (this.refs.drawpad && this.refs.drawpad.length){
+        //     const ctx = this.refs.drawpad[0].getContext("2d");
+        //     ctx.fillStyle = "#FF0000";
+            // ctx.fillRect(0, 0, 150, 75);
+        // }
         
         return attachRet;
     }
