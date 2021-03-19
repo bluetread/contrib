f75a4f9366a7ee34bc21e1ead9e068b375b6c929
anvelez26 <anvelez26@gmail.com>
2021-03-19 15:05:45 -0400

    working sketchpad component using the marker.js lib

--- src/components/ImageDraw/ImageDraw.ts
+++ src/components/ImageDraw/ImageDraw.ts
@@ -1,5 +1,6 @@
+import * as markerjs2 from "markerjs2";
 import { Components } from 'formiojs';
-import { sketchpad } from 'sketchpad-agpl/server';
+import _ from 'lodash';
 const FileComponent = (Components as any).components.file;
 
 export default class ImageDrawComponent extends FileComponent {
@@ -10,66 +11,49 @@ export default class ImageDrawComponent extends FileComponent {
           storage: 'base64'
         });
       }
-    
+
       public static editForm = FileComponent.editForm;
       public static get builderInfo() {
         return{  
-        title: 'Image Draw',
-        group: 'basic',
-        icon: 'fa fa-image',
-        weight: 70,
-        schema: ImageDrawComponent.schema()
+          title: 'Image Draw',
+          group: 'basic',
+          icon: 'pencil',
+          weight: 70,
+          schema: ImageDrawComponent.schema()
       };
     }
 
     render(){
-        let uploadRender = super.render();
-        uploadRender += '<canvas ref="drawpad" height="200px" width="200px></canvas>';
-        return uploadRender;
-        // return super.render(this.renderTemplate('imagedraw', {
-        //     fileSize: this.fileSize,
-        //     files: this.dataValue || [],
-        //     statuses: this.statuses,
-        //     disabled: this.disabled,
-        //     support: this.support,
-        //     fileDropHidden: this.fileDropHidden
-        // }));
-    }
-
-    getValue(){
-        return this.dataValue;
-    }
-
-    get emptyValue(){
-        return[];
-    }
-
-    get modes(){
-        return{
-            pencil:{
-                title: 'Pencil',
-                state:{
-                    mode: 'pencil'
-                },
-                
-            }
-        }
+      let uploadRender = super.render();
+      uploadRender += '<a ref="clickStart"><img ref="gridImage" src="https://upload.wikimedia.org/wikipedia/commons/f/f8/Numbered_14x20_grid.svg" width="400px" crossorigin="anonymous"/></a>';
+      return uploadRender;
     }
 
     attach(element){
+
         const attachRet = super.attach(element);
         this.loadRefs(element.parentNode, {
-            drawpad: 'single'
+            gridImage: 'single',
+            clickStart: 'single'
         });
-        const ctx = this.refs.drawpad.getContext("2d");
-              ctx.fillStyle = "#FF0000";
-              ctx.fillRect(0, 0, 150, 75);
-        // if (this.refs.drawpad && this.refs.drawpad.length){
-        //     const ctx = this.refs.drawpad[0].getContext("2d");
-        //     ctx.fillStyle = "#FF0000";
-            // ctx.fillRect(0, 0, 150, 75);
-        // }
+
+        this.addEventListener(this.refs.clickStart, 'click', (event) => {
+          event.preventDefault();
+          this.markerArea.show();
+        });
+
+        if (this.refs.gridImage){
+        this.markerArea = new markerjs2.MarkerArea(this.refs.gridImage);
+        this.markerArea.settings.displayMode = 'inline';
+        this.markerArea.availableMarkerTypes = this.markerArea.DEFAULT_MARKER_TYPES;
         
+        this.markerArea.addRenderEventListener((dataURL) => this.refs.gridImage.src = dataURL);
+        }
+        else{
+          this.markerArea.close();
+        }
         return attachRet;
     }
+
 }
+
