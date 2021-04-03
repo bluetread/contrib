a2438066b6f3e9757fdee420357c2d092c75fc62
anvelez26 <anvelez26@gmail.com>
2021-04-03 10:56:33 -0700

    remove arrow marker

--- src/components/ImageDraw/ImageDraw.ts
+++ src/components/ImageDraw/ImageDraw.ts
@@ -47,9 +47,11 @@ export default class ImageDrawComponent extends FileComponent {
             this.markerArea.targetRoot = document.getElementById('markerImage');
            // element.className = 'markerImage';
             this.markerArea.settings.displayMode = 'inline';
-            this.markerArea.availableMarkerTypes = this.markerArea.DEFAULT_MARKER_TYPES;
+            // this.markerArea.availableMarkerTypes = this.markerArea.DEFAULT_MARKER_TYPES;
+            this.markerArea.availableMarkerTypes = ['FrameMarker', 'FreehandMarker', 'LineMarker','TextMarker'];
             this.markerArea.settings.defaultColorSet = ['black','red', 'green', 'blue'];
             this.markerArea.settings.defaultColor = 'black';
+            this.markerArea.uiStyleSettings.
             this.markerArea.uiStyleSettings.toolbarStyleColorsClassName = 'bg-gray-50';
             this.markerArea.uiStyleSettings.toolbarButtonStyleColorsClassName =
               'bg-gradient-to-t from-gray-50 to-gray-50 hover:from-gray-50 hover:to-pink-50 fill-current text-pink-300';
